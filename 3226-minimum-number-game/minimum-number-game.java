d4e3b93f26ff4186b729693b0861d59d9793d058
Bhagyashree1234a <1nc23cs026@ncetmail.com>
2026-03-14 18:01:41 +0530

    Time: 5 ms (82.03%) | Memory: 47.3 MB (24.13%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.java
+++ 3226-minimum-number-game/minimum-number-game.java
@@ -3,9 +3,9 @@ class Solution {
         Arrays.sort(nums);
         for(int i=0;i<nums.length;i+=2)
         { 
-            nums[i]=nums[i]+nums[i+1];
-            nums[i+1]=nums[i]-nums[i+1];
-            nums[i]=nums[i]-nums[i+1];
+            int t=nums[i];
+            nums[i]=nums[i+1];
+            nums[i+1]=t;
         }
         return nums;
 
