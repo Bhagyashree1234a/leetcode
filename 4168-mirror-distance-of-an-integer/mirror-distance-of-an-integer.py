a2fa76c2f3fed814131f1814ecb24c01024a4635
Bhagyashree1234a <1nc23cs026@ncetmail.com>
2026-04-28 17:58:29 +0530

    Time: 3 ms (20.46%) | Memory: 12.5 MB (14.70%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mirrorDistance(self, n):
+        m=n
+        rev=0
+        while n>0:
+            d=n%10
+            rev=(rev*10)+d
+            n=n//10
+        return abs(m-rev)
+        
+
+
+        """
+        :type n: int
+        :rtype: int
+        """
+        
