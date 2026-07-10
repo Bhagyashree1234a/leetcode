eba3b050fa2be764b1903441d78cb63ac378c63b
Bhagyashree1234a <1nc23cs026@ncetmail.com>
2026-07-10 18:40:54 +0530

    Time: 0 ms (100.00%) | Memory: 50.1 MB (80.04%) - LeetSync

--- 4008-restore-finishing-order/restore-finishing-order.cpp
+++ 4008-restore-finishing-order/restore-finishing-order.cpp
@@ -3,16 +3,17 @@ public:
     vector<int> recoverOrder(vector<int>& order, vector<int>& friends) {
         int n = order.size();
         int m = friends.size();
-        vector<int> ans;
-        int i,j;
+        vector<int> ans(m);
+        int i,j,k=0;
         for (i = 0; i < n; i++)
         {
             for(j = 0; j < m; j++)
             {
                 if (  friends[j] == order[i] )
                 {
-                    ans.push_back(friends[j]);
-        
+                    //ans.push_back(friends[j]);
+                    ans[k]=order[i];
+                    k++;
                 }
             }
         }
