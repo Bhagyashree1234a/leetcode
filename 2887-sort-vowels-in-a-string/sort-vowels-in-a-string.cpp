afa3df9655dfaf82798bef28eb2ba4d7efe197c2
Bhagyashree1234a <1nc23cs026@ncetmail.com>
2026-07-14 17:26:35 +0530

    Time: 23 ms (21.69%) | Memory: 15.6 MB (84.20%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+
+        int n = s.size();
+        string s1 = "";
+
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+                s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+
+                s1.push_back(s[i]);
+            }
+        }
+
+        sort(s1.begin(), s1.end());
+
+        int j = 0;
+        for (int i = 0; i < n; i++) {
+            if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' ||
+                s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U') {
+
+                s[i] = s1[j];
+                j++;
+            }
+        }
+
+        return s;
+    }
+};
+
+        
