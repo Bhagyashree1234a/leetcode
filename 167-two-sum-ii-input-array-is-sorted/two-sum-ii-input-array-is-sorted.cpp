f8d548065dd916a8d22c439cab8e059040efc9d8
BHAGYASHREE <1nc23cs026@ncetmail.com>
2026-07-16 17:57:47 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (95.64%) - LeetSync

---
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i,j;
+        int n = numbers.size();
+        i = 0,j = n-1;
+        while(i < j){
+                if( numbers[i] + numbers[j] == target) {
+                    return {i+1,j+1};
+                }
+                else if (numbers[i] + numbers[j] > target) {
+                    j--;
+                }
+                else {
+                    i++;
+                }
+
+            }
+        
+        return {};
+        
+    }
+};
