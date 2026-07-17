ee5cc81f77e1ba021bad479bd780e7eb3b9b33db
BHAGYASHREE <1nc23cs026@ncetmail.com>
2026-07-17 10:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (42.20%) - LeetSync

---
+++ 1944-truncate-sentence/truncate-sentence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string truncateSentence(string s, int k) {
+        string ans = "";
+        int sc = 0;
+        for(int  i = 0; i < s.size(); i++) { 
+            if(s[i] == ' ') {
+                sc++;
+            }
+            if(sc == k) return ans;
+            ans+=s[i]; 
+
+        }
+        return ans;
+        
+
+        
+    }
+};
