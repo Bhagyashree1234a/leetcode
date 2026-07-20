eddecbc7a3cbbd47c40b54aa514cb40dee93445c
BHAGYASHREE <1nc23cs026@ncetmail.com>
2026-07-20 13:07:31 +0530

    Time: 2 ms (7.22%) | Memory: 8.8 MB (91.85%) - LeetSync

---
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int scoreOfString(string s) {
+        int n = s.size();
+        int i;
+        int sum = 0;
+        for (i = 0; i < n-1; i++) {
+            sum+=abs((s[i]-'a')-(s[i+1]-'a'));
+        }
+        return sum;
+
+
+        
+    }
+};
