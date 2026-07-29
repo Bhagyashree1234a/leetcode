f3925c7351bcdc2e1399cbb080e175589abe0c10
BHAGYASHREE <1nc23cs026@ncetmail.com>
2026-07-29 14:16:09 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (82.57%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>st;
+        int count = 0;
+        for(auto a : s) {
+            if(a == '(') {
+                st.push(a);
+            } else {
+            if(!st.empty() && st.top() == '(') {
+              st.pop();
+
+            }
+            else {
+                st.push(a);
+            }
+            }
+        }
+        return st.size();
+        
+    }
+};
