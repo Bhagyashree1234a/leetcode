02e52c591ecb43cc80b8ab96e12a6e53ca2fee43
BHAGYASHREE <1nc23cs026@ncetmail.com>
2026-07-30 10:06:17 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (74.43%) - LeetSync

---
+++ 920-uncommon-words-from-two-sentences/uncommon-words-from-two-sentences.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<string> uncommonFromSentences(string s1, string s2) {
+        vector<string> ans;
+        unordered_map<string,int> f;
+        string str = "";
+        for(int i = 0; i < s1.size(); i++) {
+            if(s1[i] != ' ') str+=s1[i];
+            if(s1[i] == ' '|| i == s1.size()-1) {
+                f[str]++;
+                str = "";
+            }
+        }
+         for(int i = 0; i < s2.size(); i++) {
+            if(s2[i] != ' ') str+=s2[i];
+            if(s2[i] == ' '|| i == s2.size()-1) {
+                f[str]++;
+                str = "";
+            }
+        }
+        for (auto a : f) {
+            if(a.second == 1) {
+                ans.push_back(a.first);
+            }
+        }
+        return ans;
+        
+    }
+};
