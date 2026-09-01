29627c0b6e629d710ccc93a6b092ab31562187c5
BHAGYASHREE <1nc23cs026@ncetmail.com>
2026-09-01 18:09:44 +0530

    Time: 0 ms (100.00%) | Memory: 15.3 MB (37.01%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -1,10 +1,30 @@
 class Solution {
 public:
-    int search(vector<int>& nums, int target) {
-        for(int i = 0; i < nums.size(); i++) {
-            if(nums[i] == target) {
-                return i;
+    int search(vector<int>& nums, int t) {
+        int l = 0;
+        int h = nums.size()-1;
+        while(l <= h) {
+            int m= l+(h-l)/2;
+            if (nums[m] == t) {
+                return m;
             }
+            if (nums[l] <= nums[m]) {
+                if(nums[l] <= t && t < nums[m] ) {
+                    h = m-1;
+                }
+                else {
+                    l = m+1;
+                }
+            } 
+            else {
+                if(nums[m] < t && t <= nums[h]) {
+                    l = m+1;
+                }
+                else {
+                    h = m-1;
+                }
+            }
+           
         }
         return -1;
         
