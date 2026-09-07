fbb98402b0674d34934537f8d802d39fcc8ff049
BHAGYASHREE <1nc23cs026@ncetmail.com>
2026-09-07 14:50:37 +0530

    Time: 3 ms (16.54%) | Memory: 28.1 MB (89.80%) - LeetSync

---
+++ 3778-transform-array-by-parity/transform-array-by-parity.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> transformArray(vector<int>& nums) {
+        vector<int>ans;
+        for(auto a : nums) {
+            if(a % 2 == 0) {
+                ans.push_back(0);
+            }
+            else {
+                ans.push_back(1);
+            }
+        }
+    
+        sort(ans.begin(), ans.end());
+        return ans;
+        
+    }
+};
